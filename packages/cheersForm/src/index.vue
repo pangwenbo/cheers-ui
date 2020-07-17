1eb017540193803d3157e6ced2c709ffa263b96f
pangwenbo <102557135@qq.com>
2020-07-17 23:14:13 +0800

    0.3.1

--- packages/cheersForm/src/index.vue
+++ packages/cheersForm/src/index.vue
@@ -3,67 +3,72 @@
 		<el-row v-for="(row,index) in fieldList" :key="index+'row'">
 			<el-col v-for="(col,index) in row.row" :key="index+'col'" :span="col.col">
 				<cheers-form-title :bdcolor="bdcolor" v-if="col.title">{{col.title}}</cheers-form-title>
-				<el-form-item v-for="item in col.list" :label="item.label" :key="item.value" :value="item.value"
-					:prop="item.value" :rules="item.rules">
-					<!-- solt -->
-					<template v-if="item.type === 'slot'">
-						<slot :name="'form-' + item.solt" />
-					</template>
-					<!-- 输入框||密码框 -->
-					<el-input v-if="item.type === 'Input'||item.type==='Password'" v-model="ruleForm[item.value]"
-						:show-password="item.type === 'Password'" :placeholder="item.placeholder||'请输入'+item.label"
-						v-bind="item.attr" />
-					<el-input-number v-if="item.type === 'NumberInput'" v-model="ruleForm[item.value]" v-bind="item.attr" />
-					<!--下拉框  $forceUpdate() 下拉刷新,修复数据改变下拉框不变的bug -->
-					<el-select v-if="item.type === 'Select'" v-model="ruleForm[item.value]" @visible-change="$forceUpdate()"
-						:placeholder=" item.placeholder||'请选择'+item.label" v-bind="item.attr"
-						@change="item.change && item.change(ruleForm[item.value])">
-						<el-option v-for=" childItem in item.options" :label="childItem[item.props.label]"
-							:value="childItem[item.props.value]" :key="childItem[item.props.value]" />
-					</el-select>
-					<!-- 三级联动 cascader -->
-					<el-cascader v-if="item.type === 'Cascader'" :options="item.options" v-model="ruleForm[item.value]"
-						:placeholder="item.placeholder ||'请选择'+item.label" :props="item.props"
-						@change="item.change &&item.change(ruleForm[item.value])" v-bind="item.attr" />
-					<!-- 单选 -->
-					<el-radio-group v-if="item.type === 'Radio'" v-model="ruleForm[item.value]"
-						@change="item.change &&item.change(ruleForm[item.value])">
-						<el-radio v-for="childItem in item.options" :label="childItem[item.props.value]"
-							:key="childItem[item.props.value]" v-bind="item.attr">
-							{{childItem[item.props.label]}}
-						</el-radio>
-					</el-radio-group>
-					<!-- 复选框 -->
-					<el-checkbox-group v-if="item.type === 'Checkbox'" v-model="ruleForm[item.value]"
-						@change="item.change &&item.change(ruleForm[item.value])">
-						<el-checkbox v-for="childItem in item.options" :key="childItem[item.props.value]"
-							:label="childItem[item.props.value]" v-bind="item.attr">{{childItem[item.props.label]}}
-						</el-checkbox>
-					</el-checkbox-group>
-					<!-- Year -->
-					<el-date-picker v-if="item.type === 'Year'" type="year" v-model="ruleForm[item.value]" value-format="yyyy"
-						format="yyyy" @change="item.change && item.change(ruleForm[item.value])"
-						:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
-					<!-- YearMonth -->
-					<el-date-picker v-if="item.type === 'YearMonth'" type="month" v-model="ruleForm[item.value]"
-						value-format="yyyy-MM" format="yyyy-MM" @change="item.change && item.change(ruleForm[item.value])"
-						:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
-					<!-- 日期 -->
-					<el-date-picker v-if="item.type === 'Date'" v-model="ruleForm[item.value]" value-format="yyyy-MM-dd"
-						format="yyyy-MM-dd" @change="item.change && item.change(ruleForm[item.value])"
-						:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
-					<!-- 日期时间 -->
-					<el-date-picker v-if="item.type === 'DateTime'" type="datetime" v-model="ruleForm[item.value]"
-						:placeholder="item.placeholder||'请选择'+item.label" value-format="yyyy-MM-dd hh:mm:ss"
-						@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
-					<!-- 起止时间 -->
-					<el-date-picker v-if="item.type === 'DateRange'" v-model="ruleForm[item.value]" type="daterange"
-						start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd"
-						@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
-					<!-- 开关 -->
-					<el-switch v-if="item.type === 'Switch'" v-model="ruleForm[item.value]"
-						@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
-				</el-form-item>
+				<template v-for="item in col.list">
+					<el-form-item v-if="f_show(item.flag||true)" :key="item.value" :label="item.label" :value="item.value"
+						:prop="item.value" :rules="item.rules">
+						<!-- solt -->
+						<template v-if="item.type === 'slot'">
+							<slot :name="'form-' + item.solt" />
+						</template>
+						<!-- 输入框-->
+						<el-input v-if="item.type==='Input'" v-model="ruleForm[item.value]"
+							:placeholder="item.placeholder||'请输入'+item.label" v-bind="item.attr" />
+						<!-- 密码框 -->
+						<el-input v-if="item.type === 'Password'" v-model="ruleForm[item.value]"
+							:show-password="item.type === 'Password'" :placeholder="item.placeholder||'请输入'+item.label"
+							v-bind="item.attr" />
+						<el-input-number v-if="item.type === 'NumberInput'" v-model="ruleForm[item.value]" v-bind="item.attr" />
+						<!--下拉框  $forceUpdate() 下拉刷新,修复数据改变下拉框不变的bug -->
+						<el-select v-if="item.type === 'Select'" v-model="ruleForm[item.value]" @visible-change="$forceUpdate()"
+							:placeholder=" item.placeholder||'请选择'+item.label" v-bind="item.attr"
+							@change="item.change && item.change(ruleForm[item.value])">
+							<el-option v-for=" childItem in item.options" :label="childItem[item.props.label]"
+								:value="childItem[item.props.value]" :key="childItem[item.props.value]" />
+						</el-select>
+						<!-- 三级联动 cascader -->
+						<el-cascader v-if="item.type === 'Cascader'" :options="item.options" v-model="ruleForm[item.value]"
+							:placeholder="item.placeholder ||'请选择'+item.label" :props="item.props"
+							@change="item.change &&item.change(ruleForm[item.value])" v-bind="item.attr" />
+						<!-- 单选 -->
+						<el-radio-group v-if="item.type === 'Radio'" v-model="ruleForm[item.value]"
+							@change="item.change &&item.change(ruleForm[item.value])">
+							<el-radio v-for="childItem in item.options" :label="childItem[item.props.value]"
+								:key="childItem[item.props.value]" v-bind="item.attr">
+								{{childItem[item.props.label]}}
+							</el-radio>
+						</el-radio-group>
+						<!-- 复选框 -->
+						<el-checkbox-group v-if="item.type === 'Checkbox'" v-model="ruleForm[item.value]"
+							@change="item.change &&item.change(ruleForm[item.value])">
+							<el-checkbox v-for="childItem in item.options" :key="childItem[item.props.value]"
+								:label="childItem[item.props.value]" v-bind="item.attr">{{childItem[item.props.label]}}
+							</el-checkbox>
+						</el-checkbox-group>
+						<!-- Year -->
+						<el-date-picker v-if="item.type === 'Year'" type="year" v-model="ruleForm[item.value]" value-format="yyyy"
+							format="yyyy" @change="item.change && item.change(ruleForm[item.value])"
+							:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
+						<!-- YearMonth -->
+						<el-date-picker v-if="item.type === 'YearMonth'" type="month" v-model="ruleForm[item.value]"
+							value-format="yyyy-MM" format="yyyy-MM" @change="item.change && item.change(ruleForm[item.value])"
+							:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
+						<!-- 日期 -->
+						<el-date-picker v-if="item.type === 'Date'" v-model="ruleForm[item.value]" value-format="yyyy-MM-dd"
+							format="yyyy-MM-dd" @change="item.change && item.change(ruleForm[item.value])"
+							:placeholder="item.placeholder||'请选择'+item.label" v-bind="item.attr" />
+						<!-- 日期时间 -->
+						<el-date-picker v-if="item.type === 'DateTime'" type="datetime" v-model="ruleForm[item.value]"
+							:placeholder="item.placeholder||'请选择'+item.label" value-format="yyyy-MM-dd hh:mm:ss"
+							@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
+						<!-- 起止时间 -->
+						<el-date-picker v-if="item.type === 'DateRange'" v-model="ruleForm[item.value]" type="daterange"
+							start-placeholder="开始日期" end-placeholder="结束日期" value-format="yyyy-MM-dd"
+							@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
+						<!-- 开关 -->
+						<el-switch v-if="item.type === 'Switch'" v-model="ruleForm[item.value]"
+							@change="item.change && item.change(ruleForm[item.value])" v-bind="item.attr" />
+					</el-form-item>
+				</template>
 			</el-col>
 		</el-row>
 		<el-form-item>
