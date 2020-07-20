55042fdc3e812f25fde8a19aeb397c500938da10
super_ <728067206@qq.com>
2020-07-20 10:53:11 +0800

    文件上传文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,61 @@
 module.exports = {
-        base: "/cheers-ui/",
-        title: "cheers-ui",
-        description: 'cheers-ui使用文档',
-        markdown: {
-                lineNumbers: true
-        },
-        head: [
-                ['link', {
-                        rel: 'icon',
-                        href: '/assets/img/logo.png'
-                }],
-                ['meta', {
-                        name: 'keywords',
-                        content: 'cheers-ui使用文档'
-                }],
-                ['meta', {
-                        name: 'author',
-                        content: '庞文博、钱华'
-                }]
-        ],
-        logo: "/assets/img/logo.png",
-        themeConfig: {
-                nav: [{
-                                text: '首页',
-                                link: '/'
-                        },
-                        {
-                                text: '文档',
-                                link: '/cheers-ui'
-                        },
-                        {
-                                text: '赞助',
-                                link: '/zanzhu'
-                        },
-                        {
-                                text: 'github',
-                                link: 'https://github.com/pangwenbo/cheers-ui'
-                        },
-                ],
-                sidebar: ['/cheers-ui', '/cheers-form', '/cheers-table', '/cheers-paginations', '/cheers-search-bar'],
-                lastUpdated: '更新时间',
-        }
-}
+  base: "/cheers-ui/",
+  title: "cheers-ui",
+  description: "cheers-ui使用文档",
+  markdown: {
+    lineNumbers: true,
+  },
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        href: "/assets/img/logo.png",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "keywords",
+        content: "cheers-ui使用文档",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "author",
+        content: "庞文博、钱华",
+      },
+    ],
+  ],
+  logo: "/assets/img/logo.png",
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "文档",
+        link: "/cheers-ui",
+      },
+      {
+        text: "赞助",
+        link: "/zanzhu",
+      },
+      {
+        text: "github",
+        link: "https://github.com/pangwenbo/cheers-ui",
+      },
+    ],
+    sidebar: [
+      "/cheers-ui",
+      "/cheers-form",
+      "/cheers-table",
+      "/cheers-paginations",
+      "/cheers-search-bar",
+      "/cheers-upload-file",
+    ],
+    lastUpdated: "更新时间",
+  },
+};
---
+++ docs/cheersUploadFile.md
@@ -0,0 +1,8 @@
+---
+sidebar: auto
+title: false
+---
+
+## cheersUploadFile 文件上传
+
+### 二级标题
